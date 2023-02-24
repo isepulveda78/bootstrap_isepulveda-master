a75ce0b46f2c88800ca7f7bdd0e208d7ba96e7d4
Israel Sepulveda <sepulveda.israel@gmail.com>
2023-02-24 16:04:07 -0500

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -2,7 +2,7 @@ import Head from "next/head"
 export default function Layout({ title, children, keywords, description }) {
 
     const background = {
-        backgroundImage: "url('./img/palmtree.svg')",
+        backgroundImage: "url('img/palmtree.svg')",
         backgroundRepeat: 'no-repeat',
         backgroundAttachment: 'fixed',
         backgroundSize: '100%',
