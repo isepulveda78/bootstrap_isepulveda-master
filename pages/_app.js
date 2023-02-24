00458bbf774a8f8dc00148ce4194d8080961d703
Israel Sepulveda <sepulveda.israel@gmail.com>
2023-02-23 21:41:00 -0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -5,8 +5,6 @@ export default function App({ Component, pageProps }) {
   return (
     <>
           <Component {...pageProps} />
-          <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossOrigin="anonymous"></script>
-          <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossOrigin="anonymous"></script>
     </>
   ) 
 }
