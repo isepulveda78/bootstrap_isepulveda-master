0675952ad9a54ce02bd23c07182d118d954a649f
Israel Sepulveda <sepulveda.israel@gmail.com>
2023-02-24 09:14:25 -0500

    Update Post.js

--- components/Post.js
+++ components/Post.js
@@ -3,6 +3,10 @@ import Moment from 'react-moment'
 import Image from "next/image"
 
 export default function Post({post}){
+    const getText = (html) => {
+        const doc = new DOMParser().parseFromString(html, "text/html")
+        return doc.body.textContent
+    }
     return (
         <div className="row mt-5">
             <div className="col-md-4">
@@ -11,11 +15,16 @@ export default function Post({post}){
        
             <div className="col-md-8">
                 <h2><Link href={post.acf.website} target="_blank" className="text-decoration-none fs-4 fw-bold text-dark">{ post.title.rendered }</Link></h2>
-                    <div>
                         <Link href={post.acf.website} target="_blank" className="text-decoration-none fs-4 fw-bold text-dark">
                             <Image src={post.thumbnail} width={1366} height={625} alt={post.title.rendered} className="card-img-top img-fluid rounded-3 shadow-lg" />
                         </Link>
-                    </div>
+                        { post.acf.description &&
+                            <div className="mt-2">
+                                { getText(post.acf.description)}
+                           </div>
+                        }
+                    
+                    
             </div>
          </div>
     )
