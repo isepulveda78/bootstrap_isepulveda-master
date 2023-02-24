906bde9cbe5a1fe25cfbc6d1836754f7e5cf9d8d
Israel Sepulveda <sepulveda.israel@gmail.com>
2023-02-24 10:36:08 -0500

    Update sitemap.xml.js

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -1,15 +1,15 @@
 //pages/sitemap.xml.js
-const EXTERNAL_DATA_URL = 'https://www.isepulveda.me/';
+const EXTERNAL_DATA_URL = 'https://jsonplaceholder.typicode.com/posts';
 
 function generateSiteMap(posts) {
   return `<?xml version="1.0" encoding="UTF-8"?>
    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
      <!--We manually set the two URLs we know already-->
      <url>
-       <loc>https://www.isepulveda.me/</loc>
+       <loc>https://jsonplaceholder.typicode.com</loc>
      </url>
      <url>
-       <loc>https://www.isepulveda.me/sideprojects</loc>
+       <loc>https://jsonplaceholder.typicode.com/guide</loc>
      </url>
      ${posts
        .map(({ id }) => {
