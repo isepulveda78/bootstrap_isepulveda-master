08b3354d1857b52b84542b663141e0c3a9648cce
Israel Sepulveda <sepulveda.israel@gmail.com>
2023-02-24 11:21:59 -0500

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -6,6 +6,7 @@ export default function Layout({ title, children, keywords, description }) {
             <title>{title}</title>
             <meta name="keywords" content={keywords} />
             <meta name="description"  content={description} />
+            <meta name="google-site-verification" content="yyYlLEoaO9AolgBQsDRzolh83aoeNvfNVBmYYoQSaNQ" />
         </Head>
         <main>
             <div className="container">
